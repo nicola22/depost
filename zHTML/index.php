cb87644cd026b4831874127c5f82840e85d5e396
nicola22 <48546148+nicola22@users.noreply.github.com>
2019-06-26 16:44:28 +0200

    Add files via upload

---
+++ zHTML/index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang = "de">
+    <head>
+        <meta charset="utf-8">
+        <link rel="stylesheet" type="Text/css" href="../css/css.css">
+        <title>Framework</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" type="Text/css" href="../Semantic/semantic.css">
+        <script src="../Semantic/semantic.js"></script>
+    </head>
+    <body>
+        <header>        
+        <div class="ui secondary pointing menu">
+            <a class="active item" href="index.html">
+                 Home
+            </a>
+            <a class="item" href="impressum.html">
+                Impressum
+            </a>
+            <a  class = "logo" href="index.html">DEPOST</a>
+            <div class="right menu">
+            <a class="ui item" href="search.html">
+                    search
+            </a>
+            <a class="ui item" href="account.html">
+                    My Account
+            </a>
+            </div>
+        </div> 
+        </header>
+        <main>
+             <h1>Home</h1>
+             <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>     
+             <div class="ui grid">
+                <div class="three wide column"><img class = "schuhe" src="../pics/Nike Air Jordan 6 Black.jpg" alt=""></div>
+                <div class="three wide column"><img class = "schuhe" src="../pics/Nike Off White Presto.jpg" alt=""></div>
+                <div class="three wide column"><img class = "schuhe" src="../pics/Nike Air Max 90 Off White Black.jpg" alt=""></div>
+                <div class="three wide column"><img class = "schuhe" src="../pics/Yeezy Boost 700 Schwarz.jpg" alt=""></div>
+            </div>
+            </main>
+    </body>
+</html>
